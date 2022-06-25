1613fcaf7e7f15cc8db5e55523c8c00e41af5390
annmark94 <annmarkova94@gmail.com>
2022-06-26 01:44:20 +0300

    Fifth commit - added the folder task8 about showing all even number from 1 to N

---
+++ Task8_allEven/Program.cs
@@ -0,0 +1,14 @@
+﻿//Show all even numbers fromm 1 to N
+Console.Write("Write your number: ");
+int yourNumber = int.Parse(Console.ReadLine());
+int currentNumber = 2;
+
+string answer = "";
+
+while (currentNumber <= yourNumber)
+{
+    answer = answer + ", " + Convert.ToString(currentNumber);
+    currentNumber = currentNumber + 2;
+}
+
+Console.WriteLine(answer.Substring(2));
