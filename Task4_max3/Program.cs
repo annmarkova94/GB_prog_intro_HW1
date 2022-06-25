ebbb527f1be5e1951258c0eac0af0d3bbcfefc39
annmark94 <annmarkova94@gmail.com>
2022-06-26 01:10:38 +0300

    Second commit - added the folder task4 about finding the maximun from three numbers

---
+++ Task4_max3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Write the first number: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Write the second number: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Write the third number: ");
+int number3 = int.Parse(Console.ReadLine());
+
+int maxim = number1;
+
+if (number2 > maxim) maxim = number2;
+if (number3 > maxim) maxim = number3;
+
+Console.WriteLine("The largest number is " + maxim);
+
