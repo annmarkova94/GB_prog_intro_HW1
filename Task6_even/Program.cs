7a031c0bffee36d7fb70e56abc8499f2aaa29e3f
annmark94 <annmarkova94@gmail.com>
2022-06-26 01:17:44 +0300

    Forth commit - added the folder task6 about finding whether the number is even or odd

---
+++ Task6_even/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Write your number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0) Console.WriteLine("Your number is even");
+else Console.WriteLine("Your number is odd");
