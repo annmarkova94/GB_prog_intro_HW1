987dbc4e7e22108cd3969504a7d8b2e065888b7e
annmark94 <annmarkova94@gmail.com>
2022-06-26 00:56:18 +0300

    First commit - added folder task2 about finding which number is larger and which is smaller out of two numbers

---
+++ Task2_max2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Find which number is larger and which is smaller out of two numbers
+Console.Write("Write the first number: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Write the second number: ");
+int number2 = int.Parse(Console.ReadLine());
+
+int result1 = number1;
+int result2 = number2;
+
+if (number2 > number1)
+{
+    result1 = number2;
+    result2 = number1;
+}
+
+Console.Write(result1 +" is larger than " + result2);
+
